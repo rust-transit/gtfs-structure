974b9a747ec2144fe15ae2c2cb16d56a82418a2e
antoine-de <antoine.desbordes@gmail.com>
2022-05-03 20:47:40 +0200

    Implement borrow and remove lots of `to_owned()` 🎉

--- src/id.rs
+++ src/id.rs
@@ -18,6 +18,8 @@ pub struct Id<T> {
 }
 
 impl<T> Id<T> {
+
+    /// private method to build an Id that exists in the [Collection]
     fn must_exists(s: String) -> Id<T> {
         Id {
             id: s,
@@ -38,6 +40,13 @@ impl<T> std::ops::Deref for Id<T> {
     }
 }
 
+// Implements Borrow to be able to look in the hashmap with just a &str instead of a String
+impl<T> std::borrow::Borrow<str> for Id<T> {
+    fn borrow(&self) -> &str {
+        &self.id
+    }
+}
+
 /// Collection with typed Ids
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Collection<T>(HashMap<Id<T>, T>);
@@ -49,32 +58,35 @@ impl<T> Default for Collection<T> {
 }
 
 impl<T> Collection<T> {
+
+    /// Get a typed [Id] from a raw &str
+    /// An [Id] can be returned only if it exists in the [Collection]
     pub fn get_id(&self, raw_id: &str) -> Option<Id<T>> {
-        let id = Id::must_exists(raw_id.to_owned());
-        if self.0.contains_key(&id) {
-            Some(id)
-        } else {
-            None
-        }
-    }
-    pub(crate) fn get_by_str(&self, raw_id: &str) -> Option<(Id<T>, &T)> {
-        let id = Id::must_exists(raw_id.to_owned());
-        self.0.get(&id).map(|v| (id, v))
+        self.get_by_str(raw_id).map(|(k, _v)| k.clone())
     }
 
+    /// Get an &[Id] and a reference to the object associated with it if it exists in the [Collection]
+    pub(crate) fn get_by_str(&self, raw_id: &str) -> Option<(&Id<T>, &T)> {
+        self.0.get_key_value(raw_id)
+    }
+    
+    /// Get an &[Id] and a mutable reference to the object associated with it if it exists in the [Collection]
     pub(crate) fn get_mut_by_str(&mut self, raw_id: &str) -> Option<(Id<T>, &mut T)> {
-        let id = Id::must_exists(raw_id.to_owned());
-        self.0.get_mut(&id).map(|v| (id, v))
+        self.0.get_mut(raw_id).map(|v| (Id::must_exists(raw_id.to_owned()), v))
     }
 
+    /// Get the object associated to the typed [Id]
     pub fn get(&self, id: &Id<T>) -> Option<&T> {
         self.0.get(id)
     }
+
+    /// Returns the number of objects in the [Collection]
     pub fn len(&self) -> usize {
         self.0.len()
     }
 }
 
+// Implements FromIterator to be able to easily build a [Collection] if we know how to associate an object with its [Id]
 impl<T: WithId> FromIterator<T> for Collection<T> {
     fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
         let mut c = Self::default();

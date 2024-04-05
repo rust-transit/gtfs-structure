8fa538de2ef7d6a100769669c5f89d2e72d2c3fd
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2024-04-05 16:19:57 -0700

    Run cargo fmt

--- src/objects.rs
+++ src/objects.rs
@@ -3,8 +3,8 @@ use crate::serde_helpers::*;
 use chrono::{Datelike, NaiveDate, Weekday};
 use rgb::RGB8;
 
-use std::hash::{Hash, Hasher};
 use std::fmt;
+use std::hash::{Hash, Hasher};
 use std::sync::Arc;
 
 /// Objects that have an identifier implement this trait
